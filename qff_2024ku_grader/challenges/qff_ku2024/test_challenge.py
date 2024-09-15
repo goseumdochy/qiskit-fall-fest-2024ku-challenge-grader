7aad6690a7067dcb14b55ef06dba9ded016faa34
goseumdochy <ssgg0926@korea.ac.kr>
2024-09-15 22:57:31 +0900

    Modify test_challenge.py typechecker

--- qff_2024ku_grader/challenges/qff_ku2024/test_challenge.py
+++ qff_2024ku_grader/challenges/qff_ku2024/test_challenge.py
@@ -1,6 +1,7 @@
 from typeguard import typechecked
 from qff_2024ku_grader.grader.grade import grade
+from qiskit import QuantumCircuit
 
 @typechecked
-def grade_test_challenge(answer: list) -> None:
+def grade_test_challenge(answer: QuantumCircuit) -> None:
     grade(answer, "test")
